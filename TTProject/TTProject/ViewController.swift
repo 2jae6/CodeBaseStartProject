f9739fa1a1898551eb05a6c19efc2a3b1c04394e
JaeWook Lee <47078140+2jae6@users.noreply.github.com>
2022-04-12 12:56:37 +0900

    Add files via upload
    Codebase Starter Pack for Study

---
+++ TTProject/TTProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TTProject
+//
+//  Created by infludeo on 2022/03/11.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = .red
+    }
+    
+   
+ 
+}
